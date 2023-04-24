4e08d102b66fdb2780a0b13a0dc495c03c07ece9
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-04-25 01:35:17 +0200

    Update myApp.js
    Searching for solution to connect and operatre on multiple records using databases.

--- myApp.js
+++ myApp.js
@@ -1,31 +1,44 @@
 require('dotenv').config();
 
+/** 1) Install & Set up mongoose */
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
+/** 2) Create a 'Person' Model */
 var personSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  favoriteFoods: [String]
+	name: String,
+	age: Number,
+	favoriteFoods: [String]
 });
 
+/** 3) Create and Save a Person */
 var Person = mongoose.model('Person', personSchema);
 
-var createAndSavePerson = function() {
-		var janeZonda = new Person({name: "Jane Zonda", age: 48, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
-		janeZonda.save(function(err, data){
-			if(err) {
-				console.error(err);
-			} else {
-				console.log(data);
-			}
-		});
-	};
-
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+var createAndSavePerson = function(done) {
+	var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+	janeFonda.save(function(err, data) {
+		if(err) return console.error(err);
+		done(null, data)
+	});
 };
 
+/** 4) Create many People with `Model.create()` */
+var arrayOfPeople = [
+	{name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+	{name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+	{name: "Robert", age: 78, favoriteFoods: ["wine"]},
+];
+
+var createManyPeople = function(arrayOfPeople, done) {
+	Person.create(arrayOfPeople, function(err, people){
+		if(err) return console.log(err);
+		done(null, people);
+	});
+};
+/** const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/ /*);
+};
+*/
 const findPeopleByName = (personName, done) => {
   done(null /*, data*/);
 };
